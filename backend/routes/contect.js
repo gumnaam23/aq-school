2bb35825a74e99618a77a4c5f5ec63b0ce494fbf
gumnaam23 <aqibdev23@gmail.com>
2025-04-27 14:57:47 +0530

    Delete backend directory

--- backend/routes/contect.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {handleContect} from '../controllers/contect.js'
-
-const router = express.Router();
-
-router.post('/', handleContect);
-
-export default router
