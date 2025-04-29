67119f7a0a1d4e0a52f152e162211919379577b1
gumnaam23 <aqibdev23@gmail.com>
2025-04-29 16:59:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import userRoute from './routes/user.js'
 import contectRoute from './routes/contect.js'
 import cookieParser from 'cookie-parser'
+import cors from "cors"
 // import {ristricToLoggedinUserOnly} from './middleware/auth.js'
 
 const app = express()
@@ -9,11 +10,14 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
+app.use(cors({
+  origin: 'https://aq-school-frontend.vercel.app', // Frontend ka URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.use('/api/user', userRoute)
 app.use('/api/contect', contectRoute)
-app.get('/', (req, res)=>{
-  res.send("hello this is backend")
-})
+
 
 export default app
