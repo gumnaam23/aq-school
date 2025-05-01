4102989047bb610098e7708f4e77a730feeb9507
gumnaam23 <aqibdev23@gmail.com>
2025-05-01 22:49:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,11 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173', // Frontend ka URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}))
 // app.use(cors({
 //   origin: 'https://aqschool.vercel.app', // Frontend ka URL
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
